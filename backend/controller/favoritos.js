49f647e0366a0ed17845551804b0089e17241b5d
Guilherme Sotoa <potaoew@gmail.com>
2021-07-14 18:52:38 -0300

    criado a requisicao de remover prato favorito

---
+++ backend/controller/favoritos.js
@@ -0,0 +1,37 @@
+const userModel = require("../model/favoritos");
+
+
+module.exports = (app) => {
+    app.get("/user/:id/favoritos", (req, res) => {
+        userModel.ListaFavoritos(req.params.id)
+            .then(favoritos => {
+                return res.json(favoritos); 
+            })
+            .catch(e =>{
+                return res.status(404).json(e)
+            })
+    })
+
+    
+    app.post("/user/:id/favoritos", async (req, res) => {
+        userModel.AdicionaFavoritos(req.params.id, req.body.id_prato)
+        .then(() => {
+            return res.json({mensagem: 'Prato adicionado aos favoritos'});
+        })
+        .catch(e => {
+            console.log(e);
+            return res.status(400).json(e)
+        })
+    })
+ 
+    app.delete("/user/:id/favoritos", (req, res) => {
+        userModel.RemoveFavoritos(req.params.id, req.body.id_prato)
+            .then(() => {
+                return res.json({mensagem: 'Prato removido dos favoritos'});
+            })
+            .catch(e => {
+                console.log(e);
+                return res.status(400).json(e)
+            })
+    })
+}
