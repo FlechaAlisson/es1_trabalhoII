e1638b28770ed07b2a6ce638d4649db2bf3bee0d
Guilherme Sotoa <potaoew@gmail.com>
2021-07-16 13:23:26 -0300

    link com host

--- backend/public/globals.js
+++ backend/public/globals.js
@@ -1,8 +1,12 @@
+
+require('dotenv').config()
+
+
 async function configPhotoPath(callback, id){
     if(typeof callback === 'function'){
         var pratos = await callback(id);
         pratos.map((prato, index) => {
-            pratos[index].photoUrl = `/servico/pratos/${pratos[index].id}/imagem`;
+            pratos[index].photoUrl = `${process.env.BACK_HOST}/servico/pratos/${pratos[index].id}/imagem`;
         })
         return pratos;
     }
